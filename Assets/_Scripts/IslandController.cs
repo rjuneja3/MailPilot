345921847636f9c5a61c01d7c7e15df5efd8a259
Rohan Juneja <rjuneja3@my.centennialcollege.ca>
2019-10-19 12:22:16 -0300

    Final

--- Assets/_Scripts/IslandController.cs
+++ Assets/_Scripts/IslandController.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/*
+Name: Rohan Juneja
+Student ID : 300987725
+Last Modified by: Rohan Juneja
+Last Modified Date: October 19th, 2019
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Util;
